c1d02e217a5b700e5691e8e1c1291c4f28ac6169
Mannkumar Pandya <mannkpandya@gmail.com>
2023-06-19 16:00:42 +0530

    added React files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const greet = () => {
+    const heading = document.getElementById("mkheading");
+    const para = document.createElement("p");
+    para.innerText = "Hi! and Namaste! from Mannkumar K. Pandya"
+    heading.append(para);
+  }
+  const [inputVariable, setinputVariable] = useState("");
+  
+  const inputUpdate = (e) => {
+    setinputVariable(e.target.value);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <div className='navbar'>
+            <img src={logo} className="App-logo" alt="logo" />
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+
+          </div>
+        </nav>
+      <div className='main'>
+          <h1 id='mkheading'>React-Todo-List</h1>
+          <input type='text' id='inp' onChange={inputUpdate}></input>
+          <button onClick={greet}>Submit</button>
+          <p>{inputVariable}</p>
+      </div>
+        
       </header>
+      
+      
     </div>
   );
 }
